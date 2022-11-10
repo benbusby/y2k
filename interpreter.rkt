b6cef7568f840a4886bb4defccc8b3fb3bd9787c
Ben Busby <contact@benbusby.com>
2022-11-10 10:21:31 -0700

    Begin refactoring from julia->racket
    I haven't been terribly happy with julia's startup time, and gave up
    pretty quickly on attempts to speed it up. The time it took to just open
    and read a file was about 0.5s, which compared to other languages feels
    pretty sluggish. I'm not sure if I was just doing something wrong, but
    either way, I'm rewriting the interpreter in a language I've wanted to
    try out for a while, Racket.
    
    Functionally this new interpreter is behind the one written in Julia --
    it can currently only interpret .space files -- but it functions a bit
    better than the Julia one. The Julia one required developers to write the
    number of spaces for the character they wanted + 1, which has been fixed
    in the Racket implementation.

---
+++ interpreter.rkt
@@ -0,0 +1,58 @@
+#lang racket/base
+(require racket/string)
+
+(define input (vector-ref (current-command-line-arguments) 0))
+(define printable " abcdefghijklmnopqrstuvwxyz")
+(define printstr "")
+
+; Mutable "mode" for switching between interpreter states
+(define mode (box 'none))
+(define pause #f)
+
+; Define commands for modifying interpreter mode
+(define (reset)
+  (cond
+    [(> (string-length printstr) 0)
+     (displayln (string-trim printstr))
+     (set! printstr "")])
+  (set-box! mode 'none))
+(define (begin-print)
+  (set-box! mode 'begin-print))
+
+; Define table for mapping program input->commands
+(define commands (make-hash))
+(hash-set! commands 0 reset)
+(hash-set! commands 1 begin-print)
+
+(define count-substring (compose length regexp-match*))
+
+(define (retrieve-char line)
+  (string (string-ref printable
+                      (count-substring " " line))))
+
+(define (end-of-exp line)
+  (and pause (= (string-length line) 0)))
+
+(define (line-iter file)
+  (let ((line (read-line file 'any)))
+    (unless (eof-object? line)
+      (cond
+        [(or (equal? (unbox mode) 'none) (end-of-exp line))
+         ((hash-ref commands (count-substring " " line)))]
+        [else 
+          (set! pause #f)
+          (cond
+            [(equal? (unbox mode) 'begin-print)
+             (set! printstr (string-append printstr (retrieve-char line)))])])
+      (cond
+        [(= (string-length line) 0)
+         (set! pause #t)])
+      (line-iter file))
+    (reset)))
+
+; Run the interpreter against the specified file or directory
+(cond
+    [(directory-exists? input) (displayln "dir")]
+    [(file-exists? input)
+      (line-iter (open-input-file input))]
+    [else (error "File not found")])
