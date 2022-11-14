662dff6566c114389a959bb59e3dd9396da60311
Ben Busby <contact@benbusby.com>
2022-11-13 17:13:50 -0700

    Implement timestamp parsing w/ 2-digit window
    The whitespace implementation didn't really make much sense. It worked
    fine, but seems pretty inefficient. The timestamp parsing, on the other
    hand, is kinda neat and would make more sense to develop further.
    
    There was an issue with MacOS dates though, which are stored using a
    signed 64 bit number and can only go up to April 2262. As a result, my
    parser can only reliably interpret 9 digit file timestamps. A 10 digit
    timestamp could run the risk of being higher than what MacOS would
    allow, so keeping it to 9 digits allows a more portable implementation.
    
    Since the max 9 digit timestamp is ~Sept 2001, I might make the decision
    to cap all timestamps at January 1 2000 and call the language Y2K. That
    way it would feel like it had a bit more meaning behind it.

--- examples/time/hello_world/init.sh
+++ examples/time/hello_world/init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-touch --date=@10906131316 01.time
-touch --date=@201241619 02.time
-touch --date=@213052800 03.time
+touch -d $(date -r 108051212 '+%Y-%m-%dT%H:%M:%S') 01.sb
+touch -d $(date -r 215002315 '+%Y-%m-%dT%H:%M:%S') 02.sb
+touch -d $(date -r 218120427 '+%Y-%m-%dT%H:%M:%S') 03.sb
--- interpreter.rkt
+++ interpreter.rkt
@@ -1,5 +1,8 @@
 #lang racket/base
+(require racket/format)
 (require racket/string)
+
+(define get-ts file-or-directory-modify-seconds)
 
 (define input (vector-ref (current-command-line-arguments) 0))
 (define ext ".sb")
@@ -8,7 +11,9 @@
 
 ; Mutable "mode" for switching between interpreter states
 (define mode (box 'none))
+(define prev-mode (box 'none))
 (define pause #f)
+(define new-file #t)
 
 ; Define commands for modifying interpreter mode
 (define (reset)
@@ -18,21 +23,30 @@
      (set! printstr "")])
   (set-box! mode 'none))
 (define (begin-print)
-  (set-box! mode 'begin-print))
+  (set-box! mode 'begin-print)
+  (set-box! prev-mode 'begin-print))
+(define (continue)
+  (set-box! mode (unbox prev-mode)))
 
 ; Define table for mapping program input->commands
+; Documented with leading 0 to match how the interpreter
+; will see it when parsing.
 (define commands (make-hash))
-(hash-set! commands 0 reset)
-(hash-set! commands 1 begin-print)
+(hash-set! commands 00 reset)
+(hash-set! commands 01 begin-print)
+(hash-set! commands 02 continue)
 
 (define count-substring (compose length regexp-match*))
 
-(define (update-printstr line)
-  (let ([char (string-ref printable (count-substring " " line))])
+(define (update-printstr idx)
+  (let ([char (string-ref printable idx)])
     (set! printstr (string-append printstr (string char)))))
 
 (define (end-of-exp line)
   (and pause (= (string-length line) 0)))
+
+(define (end-of-input cmd)
+  (and pause (= cmd 0)))
 
 (define (sb-directory-list path)
   (filter
@@ -50,7 +64,7 @@
           (set! pause #f)
           (cond
             [(equal? (unbox mode) 'begin-print)
-             (update-printstr line)]
+             (update-printstr (count-substring " " line))]
             [else (error "Unknown mode")])])
       (cond
         [(= (string-length line) 0)
@@ -58,13 +72,35 @@
       (line-iter file))
     (reset)))
 
+(define (parse-ts time-str idx)
+  (unless (= idx (string-length time-str))
+    (define cmd (string->number (substring time-str idx (+ idx 2))))
+    (cond
+      [(or (equal? (unbox mode) 'none) (end-of-input cmd) new-file)
+       ((hash-ref commands cmd))
+       (set! new-file #f)]
+      [else
+        (set! pause #f)
+        (cond
+          [(equal? (unbox mode) 'begin-print)
+           (update-printstr cmd)]
+          [else (error "Unknown mode")])])
+    (cond
+      [(= cmd 0)
+       (set! pause #t)])
+    (parse-ts time-str (+ idx 2))))
+
 ; Run the interpreter against the specified file or directory
 (cond
     [(directory-exists? input)
      (for-each
        (lambda (f)
-         (line-iter (open-input-file (build-path input f))))
-       (sb-directory-list input))]
+         (define path (build-path input f))
+         (define time-str (string-append "0" (~v (get-ts path))))
+         (parse-ts time-str 0)
+         (set! new-file #t))
+       (sb-directory-list input))
+     (reset)]
     [(file-exists? input)
       (line-iter (open-input-file input))]
     [else (error "File not found")])
