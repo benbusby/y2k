b00da1bd11da2c11139f484a006403283c1d448e
Ben Busby <contact@benbusby.com>
2022-11-17 21:44:12 -0700

    Refactor most alists to generic lists
    This introduces a small readability issue, where lists are accessed via
    an index provided in the timestamp, but it isn't as clear when reading
    through the code that that's what's happening. Will need to reassess
    whether or not this is the right approach. I sense a more major refactor
    in the works to try and finally make this readable for anyone except for
    me.

--- interpreter.scm
+++ interpreter.scm
@@ -12,9 +12,9 @@
 (define get-ts file-modification-time)
 (define input (list-ref (command-line-arguments) 0))
 (define ext-exp (irregex "^.*.y2k$"))
-(define printable " abcdefghijklmnopqrstuvwxyz!@#$%^&*()\n")
+(define printable " abcdefghijklmnopqrstuvwxyz!@#$%^&*()")
 
-; Mutable mode vars for switching between interpreter states
+;; Variables for handling different interpreter modes
 (define mode 'none)
 (define prev-mode 'none)
 (define pause #f)
@@ -34,43 +34,89 @@
   ; erased and the window size should be returned to its original size.
   (cond [hard-reset (set! prev-mode 'none) (set! next-w-size 2)]))
 
-; Define mapping of program input->commands.
-; Keys intentionally include a leading 0 to visually
-; match how the interpreter will see it when parsing.
+;; Define mapping of program input->commands.
+;; Keys intentionally include a leading 0 to visually
+;; match how the interpreter will see it when parsing.
 (define commands
-  '((00 ,reset)
-    (01 ,(lambda () (set! mode prev-mode)))    ; Continue from previous file
-    (02 ,(lambda () (update-mode 'print-str))) ; Print string to console
-    (03 ,(lambda () (update-mode 'print-var))) ; Print variable value to console
-    (04 ,(lambda () (update-mode 'set-var)))   ; Create a variable
-    (05 ,(lambda () (update-mode 'mod-var))))) ; Modify a variable
+  '(,reset ; --------------------------------- 00 : Reset
+    ,(lambda () (set! mode prev-mode)) ; ----- 01 : Continue from previous file
+    ,(lambda () (update-mode 'print-str)) ; -- 02 : Print string to console
+    ,(lambda () (update-mode 'print-var)) ; -- 03 : Print variable to console
+    ,(lambda () (update-mode 'set-var)) ; ---- 04 : Create a new variable
+    ,(lambda () (update-mode 'mod-var)))) ; -- 05 : Modify an existing variable
 
-; Establish state tracking for manipulating variables.
+;; Establish variables for tracking progress while
+;; parsing timestamp values involved with creating a
+;; new variable.
+(define new-var-step   0)
+(define new-var-id    -1)
+(define new-var-type '())
+(define new-var-steps
+  '(,(lambda (val)
+      ; Set new variable "name" (a 2-digit value to use
+      ; when referencing the variable in a program)
+      (set! new-var-id val)
+      (inc new-var-step))
+    ,(lambda (val)
+      ; Set the new variable data type
+      ; TODO -- document types
+      (set! new-var-type (cadadr (assoc val variable-types)))
+      (inc new-var-step))
+    ,(lambda (val)
+      ; Use the two digit size provided here to create and
+      ; insert the new variable.
+      (let ([new-var (make-variable val "" 0 new-var-type)])
+        (set! variables (append variables `((,new-var-id ,new-var))))
+        (set! next-w-size 1)
+
+      ; Now that we have the size, we can switch to single digit timestamp
+      ; parsing until we've parsed the full number of digits for the variable.
+      (inc new-var-step)))
+    ,(lambda (val)
+      (let ([new-var (cadr (assoc new-var-id variables))])
+        (cond
+          [(equal? (variable-type new-var) 'numeric)
+           (set-variable-str!
+             new-var
+             (string-append (variable-str new-var) (number->string val)))
+           (cond
+             [(= (string-length (variable-str new-var)) (variable-size new-var))
+              ; Convert the string value to a numeric value
+              (set-variable-val! new-var (string->number (variable-str new-var)))
+
+              ; Reset all values pertaining to the creation of new variables
+              (set!-values (new-var-step new-var-id new-var-type) (values 0 -1 '()))
+              (reset #t)])]
+          [else (error "Unsupported variable type")])))))
+
+;; Establish variables for tracking progress while
+;; parsing timestamp values involved with modifying an
+;; existing variable.
 (define mod-target-var '())
 (define mod-tmp-var '())
 (define mod-var-step  0)
 (define mod-with-other-var #f)
 (define mod-operation '())
 (define mod-var-steps
-  '((00 ,(lambda (val)
+  '(,(lambda (val)
       ; Grab which variable the user is wanting to modify
       (set! mod-target-var (cadr (assoc val variables)))
-      (inc mod-var-step)))
-    (01 ,(lambda (val)
+      (inc mod-var-step))
+    ,(lambda (val)
       ; Set the operator to use, depending on the type of the variable
       ; ("+" for numeric, "string-append" for strings)
       (set! mod-operation
         (eval (cadadr (assoc val (get-var-ops mod-target-var)))))
-      (inc mod-var-step)))
-    (02 ,(lambda (val)
+      (inc mod-var-step))
+    ,(lambda (val)
       ; Check if another variable is being used in the calculation,
       ; or a raw number/string
       (set! mod-with-other-var (= val 01))
-      (inc mod-var-step)))
-    (03 ,(lambda (val)
+      (inc mod-var-step))
+    ,(lambda (val)
       ; If another variable is being used, we can perform the
       ; calculation and return. Otherwise, we need to check the
-      ; size of the item they're wanting to use
+      ; size of the item and continue parsing.
       (cond
         [mod-with-other-var
           ; Use the code from the timestamp to access the other
@@ -89,8 +135,8 @@
           ; that will follow.
           (set! mod-tmp-var (make-variable val "" 0 (variable-type mod-target-var)))
           (set! next-w-size 1)
-          (inc mod-var-step)])))
-    (04 ,(lambda (val)
+          (inc mod-var-step)]))
+    ,(lambda (val)
       ; Process single digit input
       (cond
         [(equal? (variable-type mod-target-var) 'numeric)
@@ -105,53 +151,8 @@
                   (variable-val mod-target-var)
                   (string->number (variable-str mod-tmp-var))))
               (reset #t)])]
-        [else (error "Unsupported type")])))
+        [else (error "Unsupported type")]))
     ))
-
-
-; Establish variables for tracking progress while
-; parsing timestamp values involved with creating a
-; new variable.
-(define new-var-step   0)
-(define new-var-id    -1)
-(define new-var-type '())
-(define new-var-steps
-  '((00 ,(lambda (val)
-      ; Set new variable "name" (a 2-digit value to use
-      ; when referencing the variable in a program)
-      (set! new-var-id val)
-      (set! new-var-step (+ new-var-step 1))))
-    (01 ,(lambda (val)
-      ; Set the new variable data type
-      ; TODO -- document types
-      (set! new-var-type (cadadr (assoc val variable-types)))
-      (set! new-var-step (+ new-var-step 1))))
-    (02 ,(lambda (val)
-      ; Use the two digit size provided here to create and
-      ; insert the new variable.
-      (let ([new-var (make-variable val "" 0 new-var-type)])
-        (set! variables (append variables `((,new-var-id ,new-var))))
-        (set! next-w-size 1))
-
-      ; Now that we have the size, we can switch to single digit timestamp
-      ; parsing until we've parsed the full number of digits for the variable.
-      (set! new-var-step (+ new-var-step 1))))
-    (03 ,(lambda (val)
-      (let ([new-var (cadr (assoc new-var-id variables))])
-        (cond
-          [(equal? (variable-type new-var) 'numeric)
-           (set-variable-str!
-             new-var
-             (string-append (variable-str new-var) (number->string val)))
-           (cond
-             [(= (string-length (variable-str new-var)) (variable-size new-var))
-              ; Convert the string value to a numeric value
-              (set-variable-val! new-var (string->number (variable-str new-var)))
-
-              ; Reset all values pertaining to the creation of new variables
-              (set!-values (new-var-step new-var-id new-var-type) (values 0 -1 '()))
-              (reset #t)])]
-          [else (error "Unsupported variable type")]))))))
 
 (define (end-of-exp cmd)
   (and pause (= cmd 0)))
@@ -167,8 +168,7 @@
         ; Refer to the main commands table if a mode has not yet been set,
         ; an escape sequence was entered, or we're beginning to parse a new file.
         [(or (equal? mode 'none) (end-of-exp cmd) new-file)
-         (reset)
-         ((eval (cadadr (assoc cmd commands))))
+         ((eval (cadr (list-ref commands cmd))))
          (set! new-file #f)]
         [else
           (set! pause #f)
@@ -193,9 +193,9 @@
             ; For example, to set variable "01" to 100, you would use the following timestamp
             ; 0401020310 -> 010...
             [(equal? mode 'set-var)
-             ((eval (cadadr (assoc new-var-step new-var-steps))) cmd)]
+             ((eval (cadr (list-ref new-var-steps new-var-step))) cmd)]
             [(equal? mode 'mod-var)
-             ((eval (cadadr (assoc mod-var-step mod-var-steps))) cmd)]
+             ((eval (cadr (list-ref mod-var-steps mod-var-step))) cmd)]
             [else (error "Unknown mode")])])
       (cond
         ; If we're using the default window size, we can assume that a "00"
