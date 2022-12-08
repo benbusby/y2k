8f5ebe8baba6d0b07f913fbe83a913db5a9db3f8
Ben Busby <contact@benbusby.com>
2022-12-07 18:39:28 -0700

    Use `gtouch` for y2k file creation on macos
    There doesn't seem to be a straightforward way of specifying file
    modification time nanoseconds on mac without installing the coreutils
    package. That package allows using the GNU version of the touch command
    via gtouch, which allows nanosecond touch-ing of files.

--- cmds.sh
+++ cmds.sh
@@ -3,7 +3,7 @@
 function set_file_time() {
     case "$(uname -s)" in
         # MacOS uses a different command for modifying file time
-        Darwin*) touch -d $(date -r $1 '+%Y-%m-%dT%H:%M:%S') $2;;
+        Darwin*) gtouch --date=@$1 $2;;
         *) touch --date=@$1 $2
     esac
 }
--- examples/hello_world/init.sh
+++ examples/hello_world/init.sh
@@ -2,6 +2,5 @@
 
 source "$(dirname $(readlink -f "$0"))/../../cmds.sh"
 
-set_file_time 208051212 01.y2k
-set_file_time 115002315 02.y2k
-set_file_time 118120427 03.y2k
+set_file_time 901080512.121500231 01.y2k
+set_file_time 151812042.700000000 02.y2k
